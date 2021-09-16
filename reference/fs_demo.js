da4581215d03e1fc9d3383a77148f69a657cd619
haki9975 <elias.robert.hakim@gmail.com>
2021-09-15 19:01:48 -0700

    renameFile test

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -28,7 +28,17 @@ const path = require("path");
 // );
 
 //Read File
-fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+// fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+//Rename File
+fs.rename(
+  path.join(__dirname, "/test", "hello.txt"),
+  path.join(__dirname, "/test", "helloworld.txt"),
+  (err) => {
+    if (err) throw err;
+    console.log("file renamed");
+  }
+);
