87546a69cc1817c2520928d500a2e36a152c9cf5
haki9975 <elias.robert.hakim@gmail.com>
2021-09-15 18:53:53 -0700

    writeFile works

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const path = require("path");
 
 //Create folder
-fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
-  if (err) throw err;
-  console.log("folder created");
-});
+// fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
+//   if (err) throw err;
+//   console.log("folder created");
+// });
 
 //Create and write to file
 
