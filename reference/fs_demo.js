f036a04c661e7421269c0843980cb8bf47297f67
haki9975 <elias.robert.hakim@gmail.com>
2021-09-15 18:50:36 -0700

    Test fs.mkdir

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,4 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 //Create folder
-fs.mkdir();
+fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
+  if (err) throw err;
+  console.log("folder created");
+});
